d0ce58a1c5313c5ec05ad8883daa5e821e1290fc
Mykola Gumeniuk <mykola@gmail.com>
2026-05-09 14:54:39 +0300

    mongodb homework

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import notesController from '../controllers/notesController.js';
+
+const router = Router();
+
+router.get('/notes', notesController.getAllNotes);
+
+router.get('/notes/:noteId', notesController.getNoteById);
+
+router.post('/notes', notesController.createNote);
+
+router.delete('/notes/:noteId', notesController.deleteNote);
+
+router.patch('/notes/:noteId', notesController.updateNote);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,55 +1,41 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import pinoHttp from 'pino-http';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 dotenv.config();
 
 const app = express();
 
-const logger = pinoHttp({
-  transport: {
-    target: 'pino-pretty',
-  },
-});
-
 app.use(cors());
+
 app.use(express.json());
+
 app.use(logger);
+
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
+
+
+
+
+const bootstrap = async () => {
+  await connectMongoDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-});
+};
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
-    error: 'Internal Server Error',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
